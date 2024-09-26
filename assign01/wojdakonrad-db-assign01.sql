bd1cedcf94db7bc2e0f43fecfc5aaa86a0ead06f
Konrad Wojda <konrad.wojda@comp.com.pl>
2024-09-26 11:02:03 +0900

    add assign01

---
+++ assign01/wojdakonrad-db-assign01.sql
@@ -0,0 +1,13 @@
+use employees_db;
+
+SELECT 'WOJDA KONRAD ASSIGN-02' as 'INFO';
+
+select count(*) from employees where birth_date >= '1980-01-01';
+
+select emp_no from salaries where salary between 75000 and 80000 limit 5;
+
+select avg(salary) from salaries where salary between 75000 and 80000;
+
+select distinct emp.last_name from employees as emp join salaries as sal on emp.emp_no = sal.emp_no where sal.salary > 100000 limit 5;
+
+-- Can be also natural join
